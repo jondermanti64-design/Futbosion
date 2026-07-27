2f6084aa801cbdd0ec50f54509cfbbae18f1ed10
jondermanti64-design <jonder.manti64@gmail.com>
2026-07-27 00:45:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,48 @@ document.getElementById(id).style.display="block";
 }
 
 // Buscar partido (por ahora es una versión inicial)
-function buscarPartido(){
+function async function buscarPartido(){
+
+const nombre = document.getElementById("buscarPartido").value.trim();
+
+const resultado = document.getElementById("resultadoBusqueda");
+
+if(nombre===""){
+    resultado.innerHTML="<p>Escribe un equipo.</p>";
+    return;
+}
+
+resultado.innerHTML="<p>🔎 Buscando...</p>";
+
+const datos = await buscarEquipo(nombre);
+
+if(!datos || !datos.teams){
+
+    resultado.innerHTML="<p>❌ Equipo no encontrado.</p>";
+
+    return;
+
+}
+
+const equipo = datos.teams[0];
+
+resultado.innerHTML=`
+
+<h2>${equipo.strTeam}</h2>
+
+<img src="${equipo.strBadge}" width="120">
+
+<p><b>Liga:</b> ${equipo.strLeague}</p>
+
+<p><b>País:</b> ${equipo.strCountry}</p>
+
+<p><b>Estadio:</b> ${equipo.strStadium}</p>
+
+<p><b>Fundado:</b> ${equipo.intFormedYear}</p>
+
+`;
+
+}
 
 const partido=document.getElementById("buscarPartido").value.trim();
 
