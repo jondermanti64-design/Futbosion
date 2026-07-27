4a815d723fa05df0445779c7cce6e1fdce3faee1
jondermanti64-design <jonder.manti64@gmail.com>
2026-07-27 00:36:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 
+// Sección activa
+function mostrar(id){
+
+const secciones=document.querySelectorAll("main section");
+
+secciones.forEach(seccion=>{
+    seccion.style.display="none";
+});
+
+document.getElementById(id).style.display="block";
+
+}
+
+// Buscar partido (por ahora es una versión inicial)
+function buscarPartido(){
+
+const partido=document.getElementById("buscarPartido").value.trim();
+
+const resultado=document.getElementById("resultadoBusqueda");
+
+if(partido===""){
+    resultado.innerHTML="<p>⚠ Escribe un partido.</p>";
+    return;
+}
+
+resultado.innerHTML=`
+<h3>🔎 Buscando...</h3>
+
+<p><b>Partido:</b> ${partido}</p>
+
+<p>En la siguiente versión BetVision AI consultará TheSportsDB automáticamente.</p>
+`;
+
+}
