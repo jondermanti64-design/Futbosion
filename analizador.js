18167e07363b116c121889475be5f75d69be88d0
jondermanti64-design <jonder.manti64@gmail.com>
2026-07-27 00:47:33 -0500

    Update analizador.js

--- analizador.js
+++ analizador.js
@@ -1,1 +1,24 @@
 
+function probabilidadImplicita(cuota){
+    return (100 / cuota).toFixed(2);
+}
+
+function analizarCuotas(cuotas){
+
+    let resultados = [];
+
+    cuotas.forEach(c => {
+
+        resultados.push({
+            mercado: c.mercado,
+            cuota: c.cuota,
+            probabilidad: probabilidadImplicita(c.cuota)
+        });
+
+    });
+
+    resultados.sort((a,b)=>b.probabilidad-a.probabilidad);
+
+    return resultados;
+
+}
